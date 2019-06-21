b78be607beed91daf6e171f759e723217fa081b3
camilacintioli <camila.cintioli@gmail.com>
2019-06-21 19:22:12 -0300

    Agrego header de authorization en getTranslations

--- src/_api/index.js
+++ src/_api/index.js
@@ -1,6 +1,12 @@
 import axios from 'axios'
+import auth0Client from '../authorization/auth';
 
 export function getTranslations(lang) {
     return (
-        axios.get(`/locales/${lang}/translation.json`).then(({data}) => data))
+        axios.get(`/locales/${lang}/translation.json`, { headers: 
+            { 
+                Authorization: `Bearer ${auth0Client.getIdToken}` 
+            } 
+        })
+        .then(({ data }) => data))
 }
